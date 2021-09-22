712577e18ffff62644097445a362ede903ac18d6
kayac-chang <egg734631@gmail.com>
2021-09-22 20:09:32 +0800

    add procfile

--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,2 @@
 export * from "https://deno.land/x/websocket@v0.1.3/mod.ts";
+export { parse } from "https://deno.land/std@0.108.0/flags/mod.ts";
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import {
   WebSocketClient as Client,
   WebSocketServer as Server,
+  parse,
 } from "./deps.ts";
 
-const wss = new Server(8080);
+const PORT = parse(Deno.args).port || 8080;
+const SECOND = 1000;
+
+const wss = new Server(PORT);
 
 const list: Client[] = [];
-
-const SECOND = 1000;
 
 setInterval(() => {
   const date = new Date().toUTCString();
@@ -17,4 +19,4 @@ setInterval(() => {
 
 wss.on("connection", (ws: Client) => list.push(ws));
 
-console.log("Server Start at 8080");
+console.log(`Server Start at ${PORT}`);
